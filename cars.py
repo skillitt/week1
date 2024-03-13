72f2902f04503f33a7501760cca88389a642d418
skillitt <markdevore@outlook.com>
2024-03-13 07:51:27 -0400

    Working on week 1

---
+++ cars.py
@@ -0,0 +1,3 @@
+car1 = '\n\tFord'
+car2 = '\n\tChevy'
+print('famous cars:' , car1 , car2)
---
+++ movies.py
@@ -0,0 +1,5 @@
+movie1 = 'King Kong'
+movie2 = 'Godzilla'
+age = 25
+sentence = movie1 + ' and ' + movie2 + ' were my favorite movies when I was ' + str(age)
+print(sentence)
